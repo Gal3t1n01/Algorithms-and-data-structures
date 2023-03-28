66789f931cabb7db9edbaf463e112d504c59e4b8
Miljan <miljan2001@hotmail.com>
2023-03-28 13:39:01 +0200

    Update lv1zadAlgoritam.c

--- lv1zadAlgoritam.c
+++ lv1zadAlgoritam.c
@@ -16,14 +16,14 @@ int sekv_pret(int V[], int n, int x){
     scanf("%d", &x);
 
     for(i=0;i<n;i++){
-        if(x == V[n])
+        if(V[n] == x)
             pron = 1;
     }
 
     if(pron == 1)
-        printf("\nBroj postoji u listi: ", V[x]);
+        printf("\nBroj postoji u listi: %d", V[x]);
     else
-        printf("\nBroj ne postoji u listi: ", -1);
+        printf("\nBroj ne postoji u listi: %d", -1);
 }
 
 void sort(int V[], int n){
@@ -39,21 +39,22 @@ void sort(int V[], int n){
         }
     }
 
-    for(int i = 0; i < n;i++){
+    /*for(int i = 0; i < n;i++){
         printf("%d  ", V[i]);
-    }
-    
+    }*/
+
     printf("\n");
 }
 
 int bin_pret(int V[], int n, int x){
-    int low, high = 0;
-    while(low <= high){
+    int low = 0;
+    int high = 0;
+    while(low >= high){
         int mid = low + (high - low) / 2;
 
         if(V[mid] == x)
             return mid;
-        
+
         if(V[mid] < x)
             low = mid + 1;
         else
@@ -67,20 +68,33 @@ int main(){
 
     int n = 0;
     int x = 0;
-    int V[1000];
 
-    time_t t1, t2,t3;
+
+
+    time_t t1, t2,t3,t4,t5,t6,t7;
     t1=clock();
 
-    printf("Broj clanova polja n");
+    printf("Broj clanova polja n: ");
     scanf("%d", &n);
-
+    int *V[n];
+    *V = (int *) malloc(n * sizeof(int));
+    t3 = clock();
     gen_arr(V, n, 0, 200);
+    t4=clock();
     sekv_pret(V,n,x);
+    t5=clock();
     sort(V, n);
+    t6=clock();
     bin_pret(V, n, x);
- 
+    t7 = clock();
+
     t2=clock();
 
-    printf("Vrijeme potrebno za cijeli program: %d", t2-t1);
+    printf("\nVrijeme potrebno za cijeli program: %ld\n", t2-t1);
+    printf("\nGen: %ld\n", t3-t4);
+    printf("\nsekv: %ld\n", t4-t5);
+    printf("\nsort: %ld\n", t5-t6);
+    printf("\nbin: %ld\n", t6-t7);
+
+    free(V);
 }
